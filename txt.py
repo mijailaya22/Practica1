aa7359e370f71a5e35f9e7062bc5adf460147d99
Mijail <andresayala907@gmail.com>
2023-12-13 20:32:34 +0100

    Create txt.py

---
+++ txt.py
@@ -0,0 +1,13 @@
+def main():
+    # Pedir al usuario que ingrese dos números
+    num1 = float(input("Ingresa el primer número: "))
+    num2 = float(input("Ingresa el segundo número: "))
+
+    # Sumar los números
+    suma = num1 + num2
+
+    # Mostrar el resultado
+    print(f"La suma de {num1} y {num2} es: {suma}")
+
+if __name__ == "__main__":
+    main()
